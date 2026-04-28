fdfd26282ee76e8bc56f11deca31a03a2f597004
nivenrooprah <niven.rooprah@bbc.co.uk>
2026-04-28 15:02:53 +0100

    test

--- viz_app.py
+++ viz_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import os
 
 from plotting_functions import load_trips_df, get_stations_location, get_stations_popularity, plot_stations
 
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     st.write('test')
 
     #dataset
+
+    st.write(os.file.exists('01_london_bike_trips_enriched.csv.zip') )
     trips_df = load_trips_df('01_london_bike_trips_enriched.csv.zip')
 
     stations_loc = get_stations_location(trips_df)
