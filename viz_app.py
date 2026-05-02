8de1f887f16c9a7008509c3bb22400ef2e286c11
nivenrooprah <niven.rooprah@bbc.co.uk>
2026-05-02 17:59:35 +0100

    mdsfad'

--- viz_app.py
+++ viz_app.py
@@ -10,18 +10,25 @@ if __name__ == '__main__':
 
     st.write('test')
 
-    #dataset
-
-    st.write(os.path.exists('01_london_bike_trips_enriched.csv.zip') )
+    st.write(os.path.exists('01_london_bike_trips_enriched.csv.zip')
     trips_df = load_trips_df('01_london_bike_trips_enriched.csv.zip')
+
+
+    #creating a slider for popularity
+
+    hour = st.slider('Hour', 0, 24, value = 12, )
+
+    st.write('Hour of Day:' ,hour)
 
     stations_loc = get_stations_location(trips_df)
     stations_pop = get_stations_popularity(trips_df)
 
-    fig,ax = plt.subplots()
 
+    fig,ax = plt.subplots()
     ax = plot_stations(ax , stations_loc, 'test',  stations_pop, 'popularity', True, None)
 
     st.pyplot(fig)
+
+    #
 
 
