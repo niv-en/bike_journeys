73713a18d4ed7cebd9e94c3bc683e7ef236d0ec7
nivenrooprah <niven.rooprah@bbc.co.uk>
2026-06-04 23:43:25 +0100

    updates

--- plotting_functions.py
+++ plotting_functions.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import geopandas as gpd
 from cartopy import crs as ccrs
+from geodatasets import get_path
+import requests
+from io import StringIO
 
 import pandas as pd
 from shapely.geometry import Point, Polygon
@@ -8,7 +11,6 @@ import numpy as np
 
 import matplotlib as mpl
 import seaborn as sns
-
 from pyproj import Transformer
 
 import os
@@ -17,26 +19,78 @@ import re
 import contextily as ctx
 import numbers
 
-from zipfile import ZipFile
-import pandas as pd
+from collections import Counter
 
-def load_trips_df(path):
-    with ZipFile(path) as zf:
-        csv_files = [
-            name for name in zf.namelist()
-            if name.endswith(".csv") and not name.startswith("__MACOSX")
-        ]
+from matplotlib.patches import FancyArrowPatch, Circle
 
-        if not csv_files:
-            raise ValueError(f"No CSV found in ZIP: {zf.namelist()}")
+import shapely
 
-        # just take the first valid CSV
-        with zf.open(csv_files[0]) as f:
-            return pd.read_csv(f)
+def load_trips_df():
 
-def transform_rush_hr(trips_df: pd.DataFrame) -> None:
-    trips_df['rush_hour'] = trips_df['rush_period'].apply(lambda x: False if x == 'non_rush' else True)
+    journeys_df = pd.DataFrame()
 
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    dataset_urls = ['419JourneyDataExtract01May2025-14May2025.csv',]
+
+    for url in dataset_urls:
+        url = f'https://cycling.data.tfl.gov.uk/usage-stats/{url}'
+        response = requests.get(url, headers=headers)
+        temp_df = pd.read_csv(StringIO(response.text))
+        journeys_df = pd.concat((journeys_df, temp_df))
+
+    return journeys_df
+
+
+def preprocess_df(df, stations_df):
+    df = df.copy()
+
+    df['start_date'] = pd.to_datetime(df['Start date'], format='mixed')
+    df['end_date'] = pd.to_datetime(df['End date'], format='mixed')
+    df['start_hour'] = df['start_date'].dt.hour
+    df['end_hour'] = df['end_date'].dt.hour
+
+    df.drop(columns=['Start date', 'End date'], inplace=True)
+    # dividing by 60_000 to get to minutes
+    df['duration_mins'] = df['Total duration (ms)'] / 60_000
+
+    df = df[df['duration_mins'] > 3]
+    df = df[df['duration_mins'] < 180]
+
+    rush_hours = {7, 8, 17, 18}
+    weekdays = {1, 2, 3, 4, 5}
+
+    start_weekday, end_weekday = df.start_date.dt.weekday, df.end_date.dt.weekday
+
+    df['rush_hour'] = ((df.start_hour.isin(rush_hours) & start_weekday.isin(weekdays)) |
+                       (df.end_hour.isin(rush_hours) & end_weekday.isin(weekdays)))
+
+    df = df.merge(stations_df, left_on='Start station', right_on='name')
+
+    return df
+
+
+def load_stations_df():
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    stations_url = 'https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml'
+    response = requests.get(stations_url, headers=headers)
+    response = StringIO(response.text)
+    stations_df = pd.read_xml(response, xpath=".//station")
+
+    return stations_df
+
+def get_station_location(stations_df):
+
+    stations_df['location'] = stations_df.apply(lambda x: (x.long, x.lat), axis=1)
+    stations_loc = dict(stations_df[['name', 'location']].values)
+
+    return stations_loc
 
 def get_stations_location(trips_df : pd.DataFrame) -> dict:
 
@@ -52,13 +106,18 @@ def get_stations_popularity(trips_df : pd.DataFrame) -> dict:
     stations, counts = np.unique(trips_df[['start_station', 'end_station']].values.ravel(), return_counts=True)
     return  dict(zip(stations, counts))
 
+
 def plot_stations(ax,
                   stations_loc: dict,
                   title: str,
                   stations_mapping: dict = {},
                   mapping_label: str | None = None,
                   background: bool = None,
-                  xy_limits: tuple = None) -> None:
+                  xy_limits: tuple = None,
+                  location_labels: dict = None,
+                  size_scaling: bool = True,
+                  alpha_scaling: bool = True) -> None:
+
     transformer = Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
 
     counts = False
@@ -86,15 +145,34 @@ def plot_stations(ax,
             x_points, y_points = points_df['x'], points_df['y']
             mapping_values = points_df['vals']
 
-            sp = ax.scatter(x_points, y_points,
-                            s=5,
-                            c=mapping_values,
-                            cmap='RdBu',
+            for x, y, val in zip(x_points, y_points, mapping_values):
+                alpha = min((val - min(mapping_values)) / (max(mapping_values) - min(mapping_values)), 1)
+                alpha = alpha if alpha == alpha else 0
+
+                sp = ax.scatter(x,
+                                y,
+                                s=3 + 15 * alpha if size_scaling else 5,
+                                c=val,
+                                cmap='viridis_r',
+                                vmin=min(mapping_values),
+                                vmax=max(mapping_values),
+                                alpha=0.5 + (alpha * 0.5) if alpha_scaling else 1)
+
+            fig = ax.get_figure()
+            sp = ax.scatter(x,
+                            y,
+                            s=0,
+                            c=val,
+                            cmap='viridis_r',
                             vmin=min(mapping_values),
                             vmax=max(mapping_values),
                             alpha=1)
 
+            fig.colorbar(sp, ax=ax, label=mapping_label, location='bottom')
+
         case str():
+
+            print('categorical plot')
 
             points_df['vals'].fillna('NA')
             groups = points_df['vals'].unique()
@@ -102,14 +180,14 @@ def plot_stations(ax,
                 temp_df = points_df[points_df['vals'] == group]
                 x_points, y_points = temp_df['x'], temp_df['y']
                 sp = ax.scatter(x_points, y_points,
-                                s=5,
+                                s=2,
                                 label=group,
                                 alpha=1)
 
         case _:
 
             x_points, y_points = points_df['x'], points_df['y']
-            sp = ax.scatter(x_points, y_points, s=5, alpha=1, c='lightblue')
+            sp = ax.scatter(x_points, y_points, s=3, alpha=0, c='lightblue')
 
     if xy_limits:
         ax.set_xlim(xy_limits[0])
@@ -118,9 +196,10 @@ def plot_stations(ax,
     if background:
         ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
 
-    if stations_mapping != {}:
-        fig = ax.get_figure()
-        fig.colorbar(sp, ax=ax, label=mapping_label, location='bottom')
+    if location_labels:
+        for location, (lon, lat) in location_labels.items():
+            x, y = transformer.transform(lon, lat)
+            ax.text(x, y, s=location, fontsize=12, weight="bold")
 
     ax.set_title(title, size=16)
     ax.axis('off');
--- viz_app.py
+++ viz_app.py
@@ -4,34 +4,32 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 
-from plotting_functions import load_trips_df, get_stations_location, get_stations_popularity, plot_stations
-
+from plotting_functions import load_stations_df, load_trips_df
 if __name__ == '__main__':
 
     st.write('test')
 
-    st.write(os.path.exists('01_london_bike_trips_enriched.csv.zip'))
-    trips_df = load_trips_df('01_london_bike_trips_enriched.csv.zip')
+    stations_df = load_stations_df()
+    trips_df = load_trips_df()
 
 
-    #culd maybe plot the dataframe here
+#    st.write(os.path.exists('01_london_bike_trips_enriched.csv.zip'))
+#    trips_df = load_trips_df('01_london_bike_trips_enriched.csv.zip')
+#
+#
+#    #culd maybe plot the dataframe here
+#
+#    st.header('Popularity Of Each Station at diff Hours')
+#    hour = st.slider('Hour', 0, 23, value = 12, )
+#
+#    #filter trips by hour
+#    trips_df_hour = trips_df[trips_df['start_hour']==hour]
+#    stations_loc = get_stations_location(trips_df_hour)
+#    stations_pop = get_stations_popularity(trips_df_hour)
+#
+#
+#    fig,ax = plt.subplots()
+#    ax = plot_stations(ax , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
 
-    st.header('Popularity Of Each Station at diff Hours')
-    hour = st.slider('Hour', 0, 23, value = 12, )
-
-    st.write('Hour of Day:' ,hour)
-
-    #filter trips by hour
-    trips_df_hour = trips_df[trips_df['start_hour']==hour]
-    stations_loc = get_stations_location(trips_df_hour)
-    stations_pop = get_stations_popularity(trips_df_hour)
-
-
-    fig,ax = plt.subplots()
-    ax = plot_stations(ax , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
-
-    st.pyplot(fig)
-
-    #
 
 
