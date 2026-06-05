631aded31d9dd3b2d66120ea7ae6e5f18d69e414
nivenrooprah <niven.rooprah@bbc.co.uk>
2026-06-05 09:18:34 +0100

    .

--- viz_app.py
+++ viz_app.py
@@ -5,9 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 
-from collections import Counter
-
 from plotting_functions import load_trips_data, preprocess_df, get_stations_location, plot_stations, get_stations_popularity, plot_journeys
+
+from collections import Counter
 
 if __name__ == '__main__':
 
@@ -31,24 +31,23 @@ if __name__ == '__main__':
     stations_pop = get_stations_popularity(journeys_df_hour)
 
 
-    fig,ax = plt.subplots(2)
-    ax[0] = plot_stations(ax[0] , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
+    fig,ax = plt.subplots()
+    ax = plot_stations(ax , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
+
+    st.pyplot(fig)
 
     journey_counts = Counter([tuple(sorted(x)) for x in journeys_df_hour[['Start station', 'End station']].values])
     journeys = [x[0] for x in journey_counts.most_common(10)]
 
-
-    ax[1] = plot_journeys(ax[1],
-        journeys=journeys,
-        station_loc=stations_loc,
-        title="Top 10 Rush Hour Journeys",
-        journey_mapping=journey_counts,
-        cmap_name="viridis_r",
-        show_arrows=True,)
+    fig, ax = plt.subplots()
+    ax = plot_journeys(
+            ax,
+            journeys=journeys,
+            station_loc=stations_loc,
+            title="Top 10 Rush Hour Journeys",
+            journey_mapping=journey_counts,
+            cmap_name="viridis_r",
+            show_arrows=True)
 
     st.pyplot(fig)
-
-
-
-
 
