166d78c86d08e32e682f46d45531b3df091eefd2
nivenrooprah <niven.rooprah@bbc.co.uk>
2026-06-05 09:22:32 +0100

    .

--- viz_app.py
+++ viz_app.py
@@ -31,17 +31,16 @@ if __name__ == '__main__':
     stations_pop = get_stations_popularity(journeys_df_hour)
 
 
-    fig,ax = plt.subplots()
-    ax = plot_stations(ax , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
+    fig,ax = plt.subplots(2)
+    ax[0] = plot_stations(ax[0] , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
 
     st.pyplot(fig)
 
     journey_counts = Counter([tuple(sorted(x)) for x in journeys_df_hour[['Start station', 'End station']].values])
     journeys = [x[0] for x in journey_counts.most_common(10)]
 
-    fig, ax = plt.subplots()
-    ax = plot_journeys(
-            ax,
+    ax[1] = plot_journeys(
+            ax[1],
             journeys=journeys,
             station_loc=stations_loc,
             title="Top 10 Rush Hour Journeys",
