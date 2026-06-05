e942273e6f7f832e07299c6a9000a2b8197c2eb8
nivenrooprah <niven.rooprah@bbc.co.uk>
2026-06-05 02:41:34 +0100

    test

--- viz_app.py
+++ viz_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -10,25 +11,24 @@ if __name__ == '__main__':
 
     st.write('test')
 
-    st.write(os.path.exists('01_london_bike_trips_enriched.csv.zip'))
-    trips_df = load_trips_df('01_london_bike_trips_enriched.csv.zip')
 
+    journeys_df = load_trips_df()
+    station_df = pd.read_csv('station_medata.csv')
 
-    #culd maybe plot the dataframe here
 
     st.header('Popularity Of Each Station at diff Hours')
     hour = st.slider('Hour', 0, 23, value = 12, )
 
     #filter trips by hour
-    trips_df_hour = trips_df[trips_df['start_hour']==hour]
-    stations_loc = get_stations_location(trips_df_hour)
-    stations_pop = get_stations_popularity(trips_df_hour)
-
-
-    fig,ax = plt.subplots()
-    ax = plot_stations(ax , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
-
-    st.pyplot(fig)
+#    trips_df_hour = trips_df[trips_df['start_hour']==hour]
+#    stations_loc = get_stations_location(trips_df_hour)
+#    stations_pop = get_stations_popularity(trips_df_hour)
+#
+#
+#    fig,ax = plt.subplots()
+#    ax = plot_stations(ax , stations_loc, f'Station Popularity at {hour if hour > 10 else f"0{hour}"  }:00',  stations_pop, 'no. journeys', True, None)
+#
+#    st.pyplot(fig)
 
     #
 
